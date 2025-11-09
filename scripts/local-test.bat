4770238e3a8ade9cf885dafbd3d988687059854c
BORISONCHIK <129211728+BorisonchikYT@users.noreply.github.com>
2025-11-09 14:36:05 +0500

    Delete scripts directory

--- scripts/local-test.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Эти скрипты работают только на Linux-сервере
-echo Для локальной разработки используйте:
-echo - Live Server в VS Code
-echo - XAMPP с SSL
-echo - WSL для тестирования Linux-окружения
-pause
